d95d12ae8a31f80f763b35e607fbd1c8c3a541fd
AdilAhmedShekhani <adilaziz2009@live.com>
2023-08-31 14:19:38 +0500

    Database Connect

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const cors = require("cors");
 const { db } = require("./db/db");
-
+const { readdirSync } = require("fs");
+const { route } = require("./routes/transaction");
 const app = express();
+
 require("dotenv").config();
 
 const PORT = process.env.PORT;
@@ -11,8 +13,13 @@ const PORT = process.env.PORT;
 app.use(express.json());
 app.use(cors());
 
+//routes
+readdirSync("./routes").map((route) =>
+  app.use("/api/v1", require("./routes/" + route))
+);
+
 const server = () => {
-    db()
+  db();
   app.listen(PORT, () => {
     console.log("listening to port:", PORT);
   });
---
+++ backend/controllers/income.js
@@ -0,0 +1,29 @@
+const incomeSchema = require("../models/incomeModels");
+
+exports.addIncome = async (req, res) => {
+  const { title, amount, category, description, date } = req.body;
+
+  const income = incomeSchema({
+    title,
+    amount,
+    category,
+    description,
+    date,
+  });
+
+  try {
+    //validations
+    if (!title || !category || !description || !date) {
+      return res.status(400).json({ message: "All fields are required!" });
+    }
+    if (amount <= 0 || !amount === "number") {
+      return res
+        .status(400)
+        .json({ message: "Amount must be a positive number!" });
+    }
+    await income.save();
+    res.status(200).json({ message: "Income Added" });
+  } catch (error) {}
+
+  console.log(income);
+};
