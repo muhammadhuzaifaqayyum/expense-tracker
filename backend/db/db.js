4deae331f671aa1006c4b5c3585805eb1ff1507b
AdilAhmedShekhani <adilaziz2009@live.com>
2023-08-31 15:10:57 +0500

    MongoDb Connected

--- backend/db/db.js
+++ backend/db/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const db = async () => {
   try {
-    mongoose.set("strictQuery", true);
+    mongoose.set("strictQuery", false);
     await mongoose.connect(process.env.MONGO_URL);
     console.log("Db Connected");
   } catch (error) {
